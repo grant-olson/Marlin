17e67943b84978a648cb7b3cb999935451c6aa5b
Grant T. Olson <grant@digitaldreamlabs.com>
2021-01-03 14:47:13 -0500

    Use PETG instead of ABS on default preheats

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1503,8 +1503,8 @@
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
+#define PREHEAT_2_LABEL       "PETG"
+#define PREHEAT_2_TEMP_HOTEND 220
 #define PREHEAT_2_TEMP_BED     90
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
